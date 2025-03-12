b61959b4a7ac2ec8e2262f6984569c75a6823248
kaplax <eumenides_x@icloud.com>
2025-03-12 21:54:17 +0800

    feat: Implement menu component and MSA template fetching
    - Added a responsive menu component with toggle functionality
    - Integrated fetching of MSA templates from an external API
    - Updated application state management to handle MSA templates and YAML content
    - Enhanced UI with new CSS classes for layout and styling

--- e2e/index.tsx
+++ e2e/index.tsx
@@ -15,6 +15,51 @@ const root = document.getElementById("app");
 
 const engine = new MseEngine();
 
+interface MsaTemplate {
+  Name: string;
+  UpdatedAt: string;
+  Score: number;
+  Status: string;
+  Category: string;
+  Content: {
+    Parameters: {
+      Region: string;
+      Name: string;
+    },
+    SceneMetadata: {
+      Name: {
+        "zh-cn": string;
+        "en": string;
+      },
+      Description: {
+        "zh-cn": string;
+        "en": string;
+      },
+      Logo: string;
+    },
+    SceneProfiles: {
+      DisplayName: {
+        "zh-cn": string;
+        "en": string;
+      },
+      Template: string;
+      Documentation: {
+        "zh-cn": string;
+        "en": string;
+      },
+      Parameters: {
+        Backend: {
+          Cpu: number;
+          Memory: number;
+        }
+      },
+      Spec: {
+        Backend: string;
+      }
+    }[]
+  }
+}
+
 function App() {
 
   const [msaYaml, setMsaYaml] = useState("");
@@ -23,7 +68,30 @@ function App() {
   const [rosYaml, setRosYaml] = useState("");
   const [config, setConfig] = useState({ SceneProfiles: [] });
   const [arch, setArch] = useState("");
+  const [openMenu, setOpenMenu] = useState(false);
+  const [msaTemplates, setMsaTemplates] = useState<MsaTemplate[]>([]);
+
   const msaYamlRef = useRef(null);
+
+
+  const queryMsaTemplates = async () => {
+    const res = await fetch("https://msa-regy-config-sdivetwiec.cn-hangzhou.fcapp.run/list?page=1&pageSize=20");
+    const { Data } = await res.json();
+    setMsaTemplates(Data.Items as MsaTemplate[]);
+  };
+
+  const queryMsaYaml = async (url: string) => {
+    const res = await fetch(url);
+    const text = await res.text();
+    return text;
+  }
+
+  const queryMsaTemplateDetail = async (templateId: string) => {
+    const res = await fetch(`https://msa-regy-config-sdivetwiec.cn-hangzhou.fcapp.run/get?name=${templateId}`);
+    const { data } = await res.json();
+    console.log(data);
+  };
+
   useEffect(() => {
     fetch("/api/config").then((res) => res.json()).then(({ data }) => {
       let globalConfig = jsYaml.load(data.debugConfigContent);
@@ -32,6 +100,7 @@ function App() {
       setGlobalParameters(globalConfig.Parameters);
       setConfig(globalConfig);
     });
+    queryMsaTemplates();
   }, []);
 
   useEffect(() => {
@@ -53,17 +122,61 @@ function App() {
     }
   }, [currentScenceProfile]);
 
+  const handleMenuClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <div>
-      <header>
-        <select id="select-env">
+      <header className="flex item-center gap-4">
+        <button className="menu" onClick={handleMenuClick}>场景列表</button>
+        <select id="select-env" className="flex-1">
           {
-            config.SceneProfiles.map((item) => (
-              <option value={item.Template}>{item.DisplayName["zh-cn"]}</option>
+            config.SceneProfiles.map((item, index) => (
+              <option key={index} value={item.Template}>{item.DisplayName["zh-cn"]}</option>
             ))
           }
         </select>
       </header>
+      <section className={`menu-container ${openMenu ? "open" : "close"}`}
+        onClick={() => {
+          setOpenMenu(false);
+        }}
+      >
+        <div className="menu-content">
+          {
+            msaTemplates.map((item) => (
+              <div className="menu-item"
+                key={item.Name}
+                onClick={(evt) => {
+                  evt.stopPropagation();
+                  const content = item.Content;
+                  const current = item.Content.SceneProfiles[0];
+                  setConfig({
+                    SceneProfiles: item.Content.SceneProfiles
+                  })
+                  queryMsaYaml(current.Template).then((text) => {
+                    setMsaYaml(text);
+                    engine.parse(text, {
+                      Global: content.Parameters,
+                      Parameters: current.Parameters,
+                    }, {
+                      components
+                    }).then((parseEngine) => {
+                      const rs = parseEngine.create();
+                      const arch = parseEngine.getArchitecture();
+                      setArch(JSON.stringify(arch, null, 2));
+                      setRosYaml(rs);
+                    });
+                  });
+                }}>
+                {item.Name}
+              </div>
+            ))
+          }
+        </div>
+      </section>
       <div id="root">
         <div className="left">
           <div className="panel">
